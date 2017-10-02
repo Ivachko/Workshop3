c1dc78d27b8eb47419d8103c6a652c6cefd73607
Ivachko <strobelpierre@gmail.com>
2017-10-02 20:22:16 +0200

    Intégration page de connexion

--- src/AppBundle/AppBundle.php
+++ src/AppBundle/AppBundle.php
@@ -1,9 +1,11 @@
 <?php
-
+// src/AppBundle/AppBundle.php
 namespace AppBundle;
-
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-
 class AppBundle extends Bundle
 {
+public function getParent()
+{
+return 'FOSUserBundle';
+}
 }
